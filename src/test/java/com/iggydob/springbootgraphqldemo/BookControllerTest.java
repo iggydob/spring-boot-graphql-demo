fe4d3eac8e63632f877e92be09065cb8fe5f4e79
iggydob <valchanov.biz@gmail.com>
2024-09-12 14:26:51 +0300

    Added tests

--- src/test/java/com/iggydob/springbootgraphqldemo/BookControllerTest.java
+++ src/test/java/com/iggydob/springbootgraphqldemo/BookControllerTest.java
@@ -1,12 +1,24 @@
 package com.iggydob.springbootgraphqldemo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.graphql.GraphQlTest;
+import org.springframework.graphql.test.tester.GraphQlTester;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+@GraphQlTest(BookController.class)
 class BookControllerTest {
 
+    @Autowired
+    private GraphQlTester graphQlTester;
+
     @Test
-    void books() {
+    void canGetBooks() {
+        graphQlTester
+                .documentName("books")
+                .execute()
+                .path("books")
+                .entityList(Book.class)
+                .hasSize(4);
+
     }
 }
