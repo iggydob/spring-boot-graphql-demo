45a142c9edb342a9c81092afbda9a808dbc2b726
iggydob <valchanov.biz@gmail.com>
2024-09-12 11:39:07 +0300

    Initial commit

---
+++ src/test/java/com/iggydob/springbootgraphqldemo/SpringBootGraphqlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iggydob.springbootgraphqldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGraphqlDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
