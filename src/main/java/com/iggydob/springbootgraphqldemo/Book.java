e14cf67730f674be163fdf5c07d45d3daf01844e
iggydob <valchanov.biz@gmail.com>
2024-09-12 13:36:27 +0300

    Added Author domain

--- src/main/java/com/iggydob/springbootgraphqldemo/Book.java
+++ src/main/java/com/iggydob/springbootgraphqldemo/Book.java
@@ -4,12 +4,12 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-public record Book(Integer id, String name, Integer pageCount) {
+public record Book(Integer id, String name, Integer pageCount, Integer authorId) {
     public static List<Book> books = Arrays.asList(
-            new Book(1, "Bible", 604),
-            new Book(2, "Harry Potter", 700),
-            new Book(3, "Foobar", 100),
-            new Book(4, "Spring Boot", 344)
+            new Book(1, "Bible", 604, 1),
+            new Book(2, "Harry Potter", 700, 2),
+            new Book(3, "Foobar", 100, 3),
+            new Book(4, "Spring Boot", 344, 1)
     );
 
     public static Optional<Book> getBookById(Integer id) {
