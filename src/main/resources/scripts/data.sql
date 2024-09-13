3845c3cca4c85fb571bae45705e30ee093269dfd
iggydob <valchanov.biz@gmail.com>
2024-09-13 12:16:39 +0300

    Added SQL scripts for tables and data

---
+++ src/main/resources/scripts/data.sql
@@ -0,0 +1,15 @@
+INSERT INTO my_db.authors (id, name)
+VALUES (1, 'J. K. Rowling'),
+       (2, 'J. R. R. Tolkien');
+
+INSERT INTO my_db.books (id, title, year, authorid)
+VALUES (1, 'Philosopher''s Stone', 1997, 1),
+       (2, 'Chamber of Secrets', 1998, 1),
+       (3, 'Prisoner Of Azkaban', 1999, 1),
+       (4, 'Goblet of Fire', 2000, 1),
+       (5, 'Order of the Phoenix', 2003, 1),
+       (6, 'Half-Blood Prince', 2005, 1),
+       (7, 'Deathly Hallows', 2007, 1),
+       (8, 'The Hobbit', 1937, 2),
+       (9, 'The Children of Húrin', 2007, 2),
+       (10, 'The Silmarillion', 1977, 2);
---
+++ src/main/resources/scripts/schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE my_db.books
+(
+    id       INT          NOT NULL PRIMARY KEY,
+    title    VARCHAR(255) NOT NULL,
+    year    INT,
+    authorId INT
+);
+
+CREATE TABLE my_db.authors
+(
+    id   INT          NOT NULL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
